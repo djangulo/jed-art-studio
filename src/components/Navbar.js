40eeb46df3ea869106f3fc3ed4f3c15ec46783b2
Denis Angulo <djal@tuta.io>
2019-05-29 17:33:25 -0400

    changes in navbar, still figuring out i18n

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,187 +1,95 @@
-import React from 'react';
-import { useStaticQuery, Link } from 'gatsby';
-import github from '../img/github-icon.svg';
-import logo from '../img/logo.svg';
-import styled from '@emotion/styled';
+import React from "react";
+import { useStaticQuery, graphql, Link } from "gatsby";
+import logo from "../img/logo.svg";
 
-const TextLogo = styled(Link)`
-  font-size: 24px;
-`;
-
-const noivBar = props => {
+const Navbar = props => {
   const [state, setState] = React.useState({
     active: false,
-    navBarActiveClass: ''
+    navBarActiveClass: ""
   });
+  console.log(JSON.stringify(props, null, 2));
+
+  const toggleHamburger = () =>
+    setState({
+      active: !state.active,
+      navBarActiveClass: state.active ? "is-active" : ""
+    });
 
   const data = useStaticQuery(graphql`
     query NavbarQuery {
-      allMarkdownRemark {
-        nodes {
-          fields {
-            langKey
-          }
+      markdownRemark {
+        frontmatter {
+          language
         }
+        fields {
+          slug
+        }
+      }
+      sitePage {
+        path
       }
     }
   `);
-
-  const toggleHamburger = () => {
-    // toggle the active boolean in the state
-    setState({
-      active: !this.state.active,
-      navBarActiveClass: state.active ? 'is-active' : ''
-    });
-  };
+  const {
+    markdownRemark: {
+      fields: { slug },
+      frontmatter: { language }
+    },
+    sitePage: { path }
+  } = data;
 
   return (
     <nav
-      className="navbar is-transparent"
-      role="navigation"
-      aria-label="main-navigation"
+      className='navbar is-transparent'
+      role='navigation'
+      aria-label='main-navigation'
     >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item" title="Logo">
-            JED Art Studio
+      <div className='container'>
+        <div className='navbar-brand'>
+          <Link to={`/${language}/`} className='navbar-item' title='Logo'>
+            <img src={logo} alt='Kaldi' style={{ width: "88px" }} />
           </Link>
           {/* Hamburger menu */}
           <div
             className={`navbar-burger burger ${state.navBarActiveClass}`}
-            data-target="navMenu"
-            onClick={() => this.toggleHamburger()}
+            data-target='navMenu'
+            onClick={() => toggleHamburger()}
           >
             <span />
             <span />
             <span />
           </div>
         </div>
-        <div id="navMenu" className={`navbar-menu ${state.navBarActiveClass}`}>
-          <div className="navbar-start has-text-centered">
-            <Link className="navbar-item" to="/about">
+        <div id='navMenu' className={`navbar-menu ${state.navBarActiveClass}`}>
+          <div className='navbar-start has-text-centered'>
+            <Link className='navbar-item' to={`/${language}/about`}>
               About
             </Link>
-            <Link className="navbar-item" to="/products">
+            <Link className='navbar-item' to={`/${language}/products`}>
               Products
             </Link>
-            <Link className="navbar-item" to="/blog">
+            <Link className='navbar-item' to={`/${language}/blog`}>
               Blog
             </Link>
-            <Link className="navbar-item" to="/contact">
+            <Link className='navbar-item' to={`/${language}/contact`}>
               Contact
             </Link>
-            <Link className="navbar-item" to="/contact/examples">
+            <Link className='navbar-item' to={`/${language}/contact/examples`}>
               Form Examples
             </Link>
           </div>
-          <div className="navbar-end has-text-centered">
-            <a
-              className="navbar-item"
-              href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
+          <div className='navbar-end has-text-centered'>
+            <Link className='navbar-item' to='/en/'>
+              EN
+            </Link>
+            <Link className='navbar-item' to='/es/'>
+              ES
+            </Link>
           </div>
         </div>
       </div>
     </nav>
   );
-};
-
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-      navBarActiveClass: ''
-    };
-  }
-
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active'
-            })
-          : this.setState({
-              navBarActiveClass: ''
-            });
-      }
-    );
-  };
-
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              JED Art Studio
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    );
-  }
 };
 
 export default Navbar;
